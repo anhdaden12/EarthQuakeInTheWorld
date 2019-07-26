ef9806513c69d1a83cece70c7cad756b2c5365c2
Bao Ngoc Ngo <ngongoc480@gmail.com>
2019-07-26 15:48:10 +0700

    no message

---
+++ CollectionView_captureItem/CollectionView_captureItem/HomeCollectionViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeCollectionViewController.swift
+//  CollectionView_captureItem
+//
+//  Created by Ngoc on 7/26/19.
+//  Copyright © 2019 Ngoc. All rights reserved.
+//
+
+import UIKit
+
+
+
+class HomeCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
